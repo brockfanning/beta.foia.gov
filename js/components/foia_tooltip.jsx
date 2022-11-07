71a4c545ccb2d4a20fa7b7997ed80c62a99c52e2
Brock Fanning <brockfanning@gmail.com>
2022-11-07 22:14:07 +0000

    Example of cleaning tooltip text

--- js/components/foia_tooltip.jsx
+++ js/components/foia_tooltip.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Tooltip from 'tooltip.js';
 import PropTypes from 'prop-types';
 
+function cleanTooltip(tooltip) {
+  // TODO: strip html here
+  return tooltip;
+}
+
 class FoiaTooltip extends Component {
   componentDidMount() {
     this.tooltip = new Tooltip(this.trigger, {
@@ -14,7 +19,7 @@ class FoiaTooltip extends Component {
 
   render() {
     return (
-      <button type="button" className="tooltip-trigger button-as-link" ref={(trigger) => { this.trigger = trigger; }} title={this.props.text}>
+      <button type="button" className="tooltip-trigger button-as-link" ref={(trigger) => { this.trigger = trigger; }} title={cleanTooltip(this.props.text)} >
         <span className="visually-hidden">Tooltip</span>
       </button>
     );
